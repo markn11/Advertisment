bbbab867caca8aacecdf55896ff9a962d2572d77
markn11 <m-niles@onu.edu>
2017-04-05 08:18:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+   
+        <style>
+p1 {
+    font-size: 300%;
+    color: #ff0000;
+}
+
+p2 {
+    font-size: 80%;
+    color: #ff0000;
+}
+
+</style>
+<style>
+img.middle {
+     align: middle;
+}
+
+</style>
+    <img align="middle"src="cokebottle3.png" alt="" height="400" width="150"/>
+<p1>open happiness</p1>
+<p2>TM </p2>
+   
+        
+    </body>
+</html>
